6dc4e5ba41e4ca9bfe84a56c0e27266213587f18
Igor Aldivan <igoraldivan.profissional@gmail.com>
2026-05-29 20:59:39 -0300

    final

--- src/main/java/com/igor/sistema_hospitalar/core/config/CorsConfig.java
+++ src/main/java/com/igor/sistema_hospitalar/core/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000") // Permite acesso de origens específicas
+                .allowedOriginPatterns("*") // Permite acesso de origens específicas
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*");
     }
